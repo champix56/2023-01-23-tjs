1328ece674a0a32197a3f9c7082c0f670d006175
champix56 <desorbaix@free.fr>
2023-01-23 14:34:55 +0100

    creation de la base d'un cmp

---
+++ src/App/components/ui/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button=()=>{
+    return <button className='Button'>benjamin</button>;
+}
+/*function Button(){
+    return <button className='Button'>benjamin</button>
+}*/
+export default Button;
