23f511ac91d9ad1b2f863523cbe85ecbd396b1cc
champix56 <desorbaix@free.fr>
2023-01-23 14:39:21 +0100

    mise en oeuvre du cmp button

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-//import logo from './logo.svg';
+import Button from './components/ui/Button/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      DEMAT BREIZH
+     <hr/>
+     <Button/>
+     <Button/>
+     <Button/>
+     <Button/>
     </div>
   );
 }
