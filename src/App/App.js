9b084257666f32020245226efbcafc70e9b421a4
champix56 <desorbaix@free.fr>
2023-01-24 11:00:23 +0100

    gestion etat fonctionnel CLASS

--- src/App/App.js
+++ src/App/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import Button from './components/ui/Button/Button'
 import './App.css';
 class App extends React.PureComponent {
-  counter=0;
+  //counter=0;
+  constructor(props){
+    super(props);
+    this.state={counter:0,uneValue:"hello"};
+  }
+  componentDidUpdate(oldProps,oldState){
+    console.log('apres le changement effectif de l\'etat',this.state.counter);
+    //console.log(arguments);
+  }
   render() {
     return (
       <div className="App">
-        voici le nb de click : {this.counter}
+        voici le nb de click : {this.state.counter}
         <hr />
         <Button onButtonClick={() => {
-          this.counter--;
-          console.log(this.counter);
-          this.forceUpdate();
+          this.setState({counter:this.state.counter-1});
+          //retard de un car setstate est async
+          //console.log(this.state.counter);
         }}>Click -1</Button>
         <Button>Click +1</Button>
       </div>
