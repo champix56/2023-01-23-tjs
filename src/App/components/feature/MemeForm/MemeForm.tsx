9a085e279ea8755b6236033ae9d99a1990fa1fcd
champix56 <desorbaix@free.fr>
2023-01-24 22:58:35 +0100

    MemeForm html base

--- src/App/components/feature/MemeForm/MemeForm.tsx
+++ src/App/components/feature/MemeForm/MemeForm.tsx
@@ -1,26 +1,114 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import style from './MemeForm.module.css'
-export interface IMemeFormProps {
-    style?: {}
+import { IImage, IMeme } from "orsys-tjs-meme/dist/interfaces/common";
+import React, { useEffect, useState } from "react";
+import Button from "../../ui/Buttonts/Button";
+import style from "./MemeForm.module.css";
+
+//types
+interface IMemeFormProps {
 }
-export interface IMemeFormState { }
-export const initialState: IMemeFormState = {}
+
 const MemeForm: React.FC<IMemeFormProps> = (props) => {
-    const [state, setstate] = useState(initialState);
-    useEffect(() => {
-        //cmpdidmount
-    }, []);
-    return (
-        <div className={style.MemeForm}
-            data-testid="MemeForm"
-            style={props.style}>
-            templaName
+  return (
+    <div data-testid="MemeForm" className={style.MemeForm}>
+      <form>
+        <h1>Titre</h1>
+        <input
+          type="text"
+          id="f_titre"
+          placeholder="saisir titre"
+          value="props.meme.titre"
+        />
+        <hr />
+        <h2>Image</h2>
+        <select>
+          <option value="-1">Aucune</option>
+        </select>
+        <hr />
+        <h2>text</h2>
+        <input
+          type="text"
+          value="props.meme.text"
+        />
+        <div className={style.half}>
+          <div>
+            <label htmlFor="f_x">x:</label>
+            <br />
+            <input
+              type="number"
+              className={style.smallInput}
+              value={0}
+            />
+          </div>
+          <div>
+            <label htmlFor="f_y">y:</label>
+            <br />
+            <input
+              type="number"
+              className={style.smallInput}
+              value={0}
+            />
+          </div>
         </div>
-    );
+        <hr />
+        <label htmlFor="f_color">Couleur</label><br/>
+        <input
+          type="color"
+          id="f_color"
+          value="#000000"
+        />
+        <div className={style.half}>
+          <div>
+            <label htmlFor="f_sz">font-size:</label>
+            <br />
+            <input
+              type="number"
+              className={style.smallInput}
+              min={0}
+              value={10}
+            />
+          </div>
+          <div>
+            <label htmlFor="f_fw">font-weight:</label>
+            <br />
+            <input
+              type="number"
+              className={style.smallInput}
+              min="100"
+              step="100"
+              max="900"
+              value="props.meme.fontWeight"
+            />
+          </div>
+        </div>
+        <div className={style.half}>
+          <div>
+            <label htmlFor="f_underline">underline:</label>
+            <br />
+            <input
+              id="f_underline"
+              type="checkbox"
+              checked={false}
+            />
+          </div>
+          <div>
+            <label htmlFor="f_italic">italic:</label>
+            <br />
+            <input
+              id="f_italic"
+              type="checkbox"
+              checked={false}
+            />
+          </div>
+        </div>
+        <hr/>
+        <div className={style.half}>
+          <Button type="reset" bgColor="tomato">
+            Reset
+          </Button>
+          <Button type="submit">save</Button>
+        </div>
+      </form>
+    </div>
+  );
 };
-MemeForm.propTypes = {
-    style: PropTypes.object,
-}
-MemeForm.defaultProps = {}
 export default MemeForm;
