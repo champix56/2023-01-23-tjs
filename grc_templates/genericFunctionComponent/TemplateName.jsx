e54e10f1f4572d106b0af9915d540d44aa19ddfe
champix56 <desorbaix@free.fr>
2023-01-24 09:27:08 +0100

    generate-react-cli config / install

---
+++ grc_templates/genericFunctionComponent/TemplateName.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import style from './TemplateName.module.css'
+
+const TemplateName = (props) => {
+    return <div className={style.TemplateName} data-testid="TemplateName">templaName</div>
+};
+export default TemplateName;
