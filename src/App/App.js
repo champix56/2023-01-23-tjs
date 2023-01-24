472391fe2190ff25f72690ae8b1b2f46f335fe58
champix56 <desorbaix@free.fr>
2023-01-24 09:51:19 +0100

    passage en class

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Button from './components/ui/Button/Button'
 import './App.css';
-
-function App() {
-  let counter=0;
-  return (
-    <div className="App">
-     voici le nb de click : {counter}
-     <hr/>
-     <Button>Click -1</Button>
-     <Button>Click +1</Button>
-    </div>
-  );
+class App extends React.PureComponent {
+  counter=0;
+  render() {
+    return (
+      <div className="App">
+        voici le nb de click : {this.counter}
+        <hr />
+        <Button onButtonClick={() => {
+          this.counter--;
+          console.log(this.counter);
+        }}>Click -1</Button>
+        <Button>Click +1</Button>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   let counter = 0;
+//   return (
+//     <div className="App">
+//       voici le nb de click : {counter}
+//       <hr />
+//       <Button onButtonClick={() => {
+//         counter--;
+//         console.log(counter);
+//       }}>Click -1</Button>
+//       <Button>Click +1</Button>
+//     </div>
+//   );
+// }
 
 export default App;
