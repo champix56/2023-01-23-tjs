8dcd0b804d4d71a6416282799a30ad1307e7af49
champix56 <desorbaix@free.fr>
2023-01-24 18:07:16 +0100

    add meme vide dummy

---
+++ src/App/interfaces/dummyMeme.ts
@@ -0,0 +1,13 @@
+import {MemeInterface} from 'orsys-tjs-meme'
+export const DummyMeme: MemeInterface = {
+  titre: '',
+  text: '',
+  x: 0,
+  y: 17,
+  fontWeight: '500',
+  fontSize: 30,
+  underline: false,
+  italic: false,
+  imageId: 0,
+  color: '#000000'
+}
