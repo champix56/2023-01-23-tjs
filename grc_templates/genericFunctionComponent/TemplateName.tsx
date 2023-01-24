819780dc74a7c9870b43148f983da112af4fb111
champix56 <desorbaix@free.fr>
2023-01-24 16:56:27 +0100

    passage tmpl en ts

---
+++ grc_templates/genericFunctionComponent/TemplateName.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import style from './TemplateName.module.css'
+export interface ITemplateNameProps {
+    style?: {}
+}
+export interface ITemplateNameState { }
+export const initialState: ITemplateNameState = {}
+const TemplateName: React.FC<ITemplateNameProps> = (props) => {
+    const [state, setstate] = useState(initialState);
+    useEffect(() => {
+        //cmpdidmount
+    }, []);
+    return (
+        <div className={style.TemplateName}
+            data-testid="TemplateName"
+            style={props.style}>
+            templaName
+        </div>
+    );
+};
+TemplateName.propTypes = {
+    style: PropTypes.object,
+}
+TemplateName.defaultProps = {}
+export default TemplateName;
