3de1d1db764f2ba9f6761402459d857acf031ccc
champix56 <desorbaix@free.fr>
2023-01-24 09:27:25 +0100

    generation de composants de UI

---
+++ src/App/components/ui/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+const Footer = (props) => {
+    return <div className={style.Footer} data-testid="Footer">templaName</div>
+};
+export default Footer;
---
+++ src/App/components/ui/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import style from './Header.module.css'
+
+const Header = (props) => {
+    return <div className={style.Header} data-testid="Header">templaName</div>
+};
+export default Header;
