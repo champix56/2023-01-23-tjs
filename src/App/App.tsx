8f19951b96a153bebe4c9c7d559e1f03ab5ad6ac
champix56 <desorbaix@free.fr>
2023-01-25 11:20:22 +0100

    gestion d'etat distribué entre composants

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { MemeSVGViewer } from "orsys-tjs-meme";
+import { MemeSVGViewer, MemeInterface } from "orsys-tjs-meme";
 import { DummyMeme } from "./interfaces/dummyMeme";
 import React from "react";
 import "./App.css";
@@ -6,17 +6,25 @@ import MemeForm from "./components/feature/MemeForm/MemeForm";
 import FlexHLayout from "./components/layout/FlexHLayout/FlexHLayout";
 import Footer from "./components/ui/Footer/Footer";
 import Header from "./components/ui/Header/Header";
-interface IAppState {}
-interface IAppProps {}
+interface IAppState { meme: MemeInterface }
+interface IAppProps { }
 
 class App extends React.PureComponent<IAppProps, IAppState> {
+  constructor(props: IAppProps) {
+    super(props)
+    this.state = { meme: DummyMeme };
+  }
   render() {
     return (
       <div className="App">
         <Header />
-        <FlexHLayout style={{height:'89vh'}}>
-          <MemeSVGViewer image={undefined} meme={DummyMeme} />
-          <MemeForm />
+        <FlexHLayout style={{ height: '89vh' }}>
+          <MemeSVGViewer image={undefined} meme={this.state.meme} />
+          <MemeForm 
+            meme={this.state.meme} 
+            onMemeValueChange={(newMeme: MemeInterface) => { 
+              this.setState({ meme: newMeme }) }
+              } />
         </FlexHLayout>
         <Footer />
       </div>
