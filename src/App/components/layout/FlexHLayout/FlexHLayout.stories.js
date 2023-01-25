e84359cb5aef985ec14c3fc5cec0b8f6ebf35d56
champix56 <desorbaix@free.fr>
2023-01-25 09:50:52 +0100

    story agmenté de flexHLayout

--- src/App/components/layout/FlexHLayout/FlexHLayout.stories.js
+++ src/App/components/layout/FlexHLayout/FlexHLayout.stories.js
@@ -5,7 +5,15 @@ export default {
   title: "FlexHLayout",
 };
 
-export const Default = () => <FlexHLayout />;
+export const Default = () => (
+  <FlexHLayout style={{ padding: '8px', border: '1px solid black' }} >
+    <div style={{ border: '1px solid black', backgroundColor: '#87CEEB3f', padding: '15px', textAlign: "center" }}>
+      1er enfant etendu
+    </div>
+    <div style={{ border: '1px solid black', backgroundColor: '#FF63473f', padding: '15px', textAlign: "center" }}>
+      2nd restreint
+    </div>
+  </FlexHLayout>);
 
 Default.story = {
   name: 'default',
